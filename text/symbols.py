2fbdd2cdac7ab2a398b7195d2c7449a0f0c9c0e3
Fiononana-Jaofera <fiononana.andriatsalama@gmail.com>
2024-11-19 06:50:54 +0300

    update

--- text/symbols.py
+++ text/symbols.py
@@ -1,16 +1,15 @@
 """ from https://github.com/keithito/tacotron """
 
-'''
+"""
 Defines the set of symbols used in text input to the model.
-'''
-_pad        = '_'
-_punctuation = ';:,.!?¡¿—…"«»“” '
-_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-_letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
-
+"""
+_pad = "_"
+_punctuation = ';:,.!?-"«»“”\' '
+_letters = "abdefghijklmnoôprstvyz"
+_letters_ipa = "d͡z#u#t͡s#ⁿt͡s#ⁿd͡z#ʈ͡ʂ#ɖ͡ʐ#ⁿʈ͡ʂ#ⁿɖ͡ʐ#ᵑk#ᵑɡ#ᵐp#ᵐb#ⁿt#ⁿd#ɵ"
 
 # Export all symbols:
-symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
+symbols = [_pad] + list(_punctuation) + list(_letters) + _letters_ipa.split("#")
 
 # Special symbol ids
 SPACE_ID = symbols.index(" ")
